5b864f0c6536a6c20d2cc9096f1c178bea8f0481
Ryan Mulligan <rmulligan@tracell.com>
2013-02-14 23:27:01 -0800

    updated deploy

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+set :application, "Everdo"
+set :repository,  "git@bitbucket.org:mulligan/everdo.git"
+set :scm, :git
+set :rvm_ruby_string, '1.9.2@everdo'
+set :rvm_type, :user
+set :branch, 'master'
+
+# =============================================================================
+# ROLES
+# =============================================================================
+role :web, "50.56.172.226"
+role :app, "50.56.172.226"
+role :db,  "50.56.172.226", :primary => true
+
+# =============================================================================
+# OPTIONAL VARIABLES
+# =============================================================================
+set :deploy_to, "/var/rails/everdo"
+set :user, "rmulligan"  # CHANGE THIS LINE TO USE YOUR OCS USERNAME
+set :use_sudo, false
+set :deploy_via, :export
+set :rails_env,      "production"

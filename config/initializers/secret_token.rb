e75356981a54484b39bc95edb2f3801bba7832ef
rmulligan <rmulligan@everdo.(none)>
2013-02-15 06:54:23 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Everdo::Application.config.secret_token = 'cc8a03912d2fa4eac1181ead5ff165ff6cdc5c27d9503e73cd1d70fb0d9a3205a61d3318fbab72201e2897fae9452e0d16a0346e1551b79fa40d3be5748df9b0'
